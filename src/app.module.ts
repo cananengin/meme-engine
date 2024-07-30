3a2cfae12a5ebfc334d1f0ed93deebfee5c4c77c
Canan Engin <cananengin@192.168.1.9>
2024-07-30 16:31:09 +0300

    removed mongodb url

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,7 +6,7 @@ import { MemeModule } from './memes/meme.module';
 
 @Module({
   imports: [
-    MongooseModule.forRoot('mongodb+srv://cananengin:129581185cnn!@cluster0.qrnhv.mongodb.net/'),
+    MongooseModule.forRoot('mongodb://localhost:27017/meme-engine'),
     MemeModule,
   ],
   controllers: [AppController],
